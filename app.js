51058810e107ed611602d701c1d7c5bb6bf3a21a
BarbDMC <barbaradmorantesc@gmail.com>
2023-10-20 14:23:02 -0500

    endpoints created

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+const port = 3000;
+
+const filters = require('./utils/filters');
+
+app.get('/:org/popularRepos', async (req, res) => {
+  const { org } = req.query;
+  const repositories = await getRepositories(org);
+
+  const popularRepos = filters.popularRepos(repositories);
+
+  res.json({ popularRepos });
+});
+
+app.get('/:org/lastUpdatedRepos', async (req, res) => {
+  const { org } = req.query;
+  const repositories = await getRepositories(org);
+
+  const lastUpdatedRepos = filters.lastUpdatedRepos(repositories);
+
+  res.json({ lastUpdatedRepos });
+});
+
+app.get('/:org/totalStars', async (req, res) => {
+  const { org } = req.query;
+  const repositories = await getRepositories(org);
+
+  const totalStars = filters.totalStars(repositories);
+
+  res.json({ totalStars });
+});
+
+async function getRepositories(org) {
+  const response = await axios.get(`https://api.github.com/orgs/${org}/repos`);
+  return response.data;
+}
+
+app.listen(port);
+
+module.exports = app;
---
+++ specs/app.spec.js
@@ -0,0 +1,47 @@
+describe('App', () => {
+  const app = require('../app.js');
+  const request = require('supertest');
+  const axios = require('axios');
+
+  jest.mock('axios');
+
+  axios.get.mockResolvedValue({
+    data: [
+      { name: 'repo1', stargazers_count: 6 },
+      { name: 'repo2', stargazers_count: 3 }
+    ],
+  });
+
+  describe('When GET /:org/popularRepos', () => {
+    it('should respond with 200 status and popular repositories', async () => {
+      const response = await request(app)
+        .get("/StackBuilders/popularRepos")
+        .set("content-type", "application/json");
+
+      expect(response.status).toBe(200);
+      expect(response.body.popularRepos).toHaveLength(1);
+    });
+  });
+
+  describe('When GET /:org/lastUpdatedRepos', () => {
+    it('should respond with 200 status and last 5 updated repositories', async () => {
+      const response = await request(app)
+        .get("/StackBuilders/lastUpdatedRepos")
+        .set("content-type", "application/json");
+
+      expect(response.status).toBe(200);
+      expect(response.body.lastUpdatedRepos).toHaveLength(2);
+    });
+  });
+
+  describe('When GET /:org/totalStars', () => {
+    it('should respond with 200 status and the stars sum', async () => {
+      const response = await request(app)
+        .get("/StackBuilders/totalStars")
+        .set("content-type", "application/json");
+
+      expect(response.status).toBe(200);
+      expect(response.body.totalStars).toBe(9);
+    });
+  });
+});
