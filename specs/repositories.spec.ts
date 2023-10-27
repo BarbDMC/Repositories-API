b202ad0be77e1b32524aa67e8f98ae5a96cba3f5
BarbDMC <barbaradmorantesc@gmail.com>
2023-10-27 10:02:47 -0500

    repositories controller specs moved to their own spec file

---
+++ specs/repositories.spec.ts
@@ -0,0 +1,41 @@
+import { jest } from '@jest/globals';
+import axios from 'axios';
+import { getPopularRepos, getLastUpdatedRepos, getTotalStars } from '../controllers/repositories';
+
+describe('Repositories Controller', () => {
+  const mockAxiosGet = jest.spyOn(axios, 'get');
+
+  mockAxiosGet.mockResolvedValue({
+    data: [
+      { name: 'repo1', stargazers_count: 6 },
+      { name: 'repo2', stargazers_count: 3 },
+    ],
+  });
+
+  describe('When getPopularRepos', () => {
+    fit('should respond with 200 status and popular repositories', async () => {
+      const popularRepos = await getPopularRepos('StackBuilders');
+
+      expect(axios.get).toHaveBeenCalledWith('https://api.github.com/orgs/StackBuilders/repos');
+      expect(popularRepos).toHaveLength(1);
+    });
+  });
+
+  describe('When getLastUpdatedRepos', () => {
+    it('should respond with 200 status and last 5 updated repositories', async () => {
+      const lastUpdatedRepos = await getLastUpdatedRepos('StackBuilders');
+
+      expect(axios.get).toHaveBeenCalledWith('https://api.github.com/orgs/StackBuilders/repos');
+      expect(lastUpdatedRepos).toHaveLength(2);
+    });
+  });
+
+  describe('When getTotalStars', () => {
+    it('should respond with 200 status and the stars sum', async () => {
+      const totalStarsRepos = await getTotalStars('StackBuilders');
+
+      expect(axios.get).toHaveBeenCalledWith('https://api.github.com/orgs/StackBuilders/repos');
+      expect(totalStarsRepos).toBe(9);
+    });
+  });
+});
