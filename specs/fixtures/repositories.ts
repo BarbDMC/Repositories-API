e1ec72c4ad124f2d8449cbe9e2eb0fc1fd4d75c9
BarbDMC <barbaradmorantesc@gmail.com>
2023-11-10 09:21:19 -0500

    Change that broke the specs

--- specs/fixtures/repositories.ts
+++ specs/fixtures/repositories.ts
@@ -5,5 +5,5 @@ export const repositories = [
   { name: 'hrepo3', stargazersCount: 20, updatedAt: '2023-10-20T10:15:00Z' },
   { name: 'repo4', stargazersCount: 12, updatedAt: '2023-10-20T10:15:00Z' },
   { name: 'repo5', stargazersCount: 7, updatedAt: '2023-10-20T10:15:00Z' },
-  { name: 'repo6', stargazersCount: 14, updatedAt: '2023-10-20T10:15:00Z' }
+  { name: 'repo6', stargazersCount: 14, updatedAt: '2023-10-15T19:30:00Z' }
 ];
